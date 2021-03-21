6bf40e765d04bf3a5e5cbb80dc099ad65b024c0c
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-03-21 22:42:26 +0100

    modify EntityScan annotation and add request.http file to test my api

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.example.ecommerce.model.persistence.repositories")
-@EntityScan("com.example.demo.model.persistence")
+@EntityScan("com.example.ecommerce.model.persistence")
 @SpringBootApplication
 public class EcommerceApplication {
 
