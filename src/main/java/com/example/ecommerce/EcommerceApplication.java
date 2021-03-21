6d8306fc6bb013bcf068bd2c905579c49ab9ddea
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-03-21 22:20:33 +0100

    add properties

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -2,7 +2,11 @@ package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.example.ecommerce.model.persistence.repositories")
+@EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication
 public class EcommerceApplication {
 
