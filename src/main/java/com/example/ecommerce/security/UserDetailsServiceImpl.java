aa22f7a77367da9dfabdd4de12015fa228ecc6ec
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-03-22 15:48:14 +0100

    security JWT

---
+++ src/main/java/com/example/ecommerce/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.ecommerce.security;
+
+import com.example.ecommerce.model.persistence.User;
+import com.example.ecommerce.model.persistence.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.emptyList());
+    }
+}
