4514a27999a8243c0bbd9ee44436672382a85b1e
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-03-22 12:34:28 +0100

    add pssword attribute to login and signup , exlude SecurityAutoConfiguration

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -3,11 +3,12 @@ package com.example.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.example.ecommerce.model.persistence.repositories")
 @EntityScan("com.example.ecommerce.model.persistence")
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/ecommerce/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/ecommerce/model/requests/CreateUserRequest.java
@@ -7,11 +7,34 @@ public class CreateUserRequest {
 	@JsonProperty
 	private String username;
 
+	@JsonProperty
+	private String password;
+
+	@JsonProperty
+	private String confirmePassword;
+
+
 	public String getUsername() {
 		return username;
 	}
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmePassword() {
+		return confirmePassword;
+	}
+
+	public void setConfirmePassword(String confirmePassword) {
+		this.confirmePassword = confirmePassword;
 	}
 }
