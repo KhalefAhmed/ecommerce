d36d9bace24c1b22cb901797efc3b68355a1721d
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-04-22 00:41:12 +0200

    add validation annotations in CreateUserRequest

--- src/main/java/com/example/ecommerce/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/ecommerce/model/requests/CreateUserRequest.java
@@ -2,14 +2,22 @@ package com.example.ecommerce.model.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+
 public class CreateUserRequest {
 
+	@NotEmpty(message = "Username must not be null or blank.")
 	@JsonProperty
 	private String username;
 
+	@NotEmpty(message = "itemId must not be null or blank.")
+	@Positive(message = "itemId must be greater than 0")
 	@JsonProperty
 	private String password;
 
+	@NotEmpty(message = "quantity must not be null or blank.")
+	@Positive(message = "quantity must be greater than 0")
 	@JsonProperty
 	private String confirmPassword;
 
