7fd0e7180672bc9f7145cdb886969b71ff60c514
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-04-28 02:19:44 +0200

    log error UserController Post /create

--- src/main/java/com/example/ecommerce/controllers/UserController.java
+++ src/main/java/com/example/ecommerce/controllers/UserController.java
@@ -48,7 +48,7 @@ public class UserController {
 
 		if(createUserRequest.getPassword().length()<7 ||
 				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
-			System.out.println("Error - Either length is less than 7 or pass and conf pass do not match. Unable to create "+
+			log.error("Error - Either length is less than 7 or pass and conf pass do not match. Unable to create "+
 					createUserRequest.getUsername());
 			return ResponseEntity.badRequest().build();
 		}
