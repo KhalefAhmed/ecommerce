304445b3f18523ac419b1458c5002ffdbd1efe66
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-04-30 15:47:11 +0200

    InvalidUserIdException

---
+++ src/main/java/com/example/ecommerce/exception/InvalidUserIdException.java
@@ -0,0 +1,10 @@
+package com.example.ecommerce.exception;
+
+public class InvalidUserIdException extends RuntimeException {
+
+    private static final String EXCEPTION_MESSAGE = "Invalid user id.";
+
+    public InvalidUserIdException() {
+        super(EXCEPTION_MESSAGE);
+    }
+}
