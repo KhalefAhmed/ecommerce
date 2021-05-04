d7eb1bd91b741d4da31eb7bbd433839d2e0ba6c9
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-05-04 16:56:56 +0200

    handle Password Validation Exception

--- src/main/java/com/example/ecommerce/errors/ErrorController.java
+++ src/main/java/com/example/ecommerce/errors/ErrorController.java
@@ -1,4 +1,12 @@
 package com.example.ecommerce.errors;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
+
 public class ErrorController extends ExceptionInInitializerError{
+
+    public final ResponseEntity<Object> handlePasswordValidationException(PasswordValidationException ex, WebRequest request) {
+        logger.error(LogMF.format("handlePasswordValidationException", ex.getLocalizedMessage(), ex));
+        return handleExceptionMessageOnly(ex, request);
+    }
 }
