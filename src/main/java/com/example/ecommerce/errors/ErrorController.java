12e42d9bdfb2a80440ce1f9874528289cabb2921
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-05-06 19:29:25 +0200

    DEFAULT_VALIDATION_FAILED_MESSAGE

--- src/main/java/com/example/ecommerce/errors/ErrorController.java
+++ src/main/java/com/example/ecommerce/errors/ErrorController.java
@@ -5,6 +5,9 @@ import org.springframework.web.context.request.WebRequest;
 
 public class ErrorController extends ExceptionInInitializerError{
 
+    private static final String DEFAULT_VALIDATION_FAILED_MESSAGE = "Validation failed.";
+
+
     public final ResponseEntity<Object> handlePasswordValidationException(PasswordValidationException ex, WebRequest request) {
         logger.error(LogMF.format("handlePasswordValidationException", ex.getLocalizedMessage(), ex));
         return handleExceptionMessageOnly(ex, request);
@@ -23,5 +26,6 @@ public class ErrorController extends ExceptionInInitializerError{
     public final ResponseEntity<Object> handleInvalidUserIdException(InvalidUserIdException ex, WebRequest request) {
         logger.error(LogMF.format("handleInvalidUserIdException", ex.getLocalizedMessage(), ex));
         return handleExceptionMessageOnly(ex, request);
-
     }
+
+}
