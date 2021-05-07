8c322b3cabe19f49a5dda7b14dce9e7f604fa3b5
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-05-08 00:26:37 +0200

    fix error in errorController

--- src/main/java/com/example/ecommerce/errors/ErrorController.java
+++ src/main/java/com/example/ecommerce/errors/ErrorController.java
@@ -8,24 +8,6 @@ public class ErrorController extends ExceptionInInitializerError{
     private static final String DEFAULT_VALIDATION_FAILED_MESSAGE = "Validation failed.";
 
 
-    public final ResponseEntity<Object> handlePasswordValidationException(PasswordValidationException ex, WebRequest request) {
-        logger.error(LogMF.format("handlePasswordValidationException", ex.getLocalizedMessage(), ex));
-        return handleExceptionMessageOnly(ex, request);
-    }
 
-    public final ResponseEntity<Object> handleUsernameExistsException(UsernameExistsException ex, WebRequest request) {
-        logger.error(LogMF.format("handleUsernameExistsException", ex.getLocalizedMessage(), ex));
-        return handleExceptionMessageOnly(ex, request);
-    }
-
-    public final ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException ex, WebRequest request) {
-        logger.error(LogMF.format("handleUsernameNotFoundException", ex.getLocalizedMessage(), ex));
-        return handleExceptionMessageOnly(ex, request);
-    }
-
-    public final ResponseEntity<Object> handleInvalidUserIdException(InvalidUserIdException ex, WebRequest request) {
-        logger.error(LogMF.format("handleInvalidUserIdException", ex.getLocalizedMessage(), ex));
-        return handleExceptionMessageOnly(ex, request);
-    }
 
 }
