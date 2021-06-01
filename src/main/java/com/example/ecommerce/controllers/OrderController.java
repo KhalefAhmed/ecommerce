bd2960f1e4ed78c0188417ef337815ac00bbd6b1
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-01 22:48:09 +0200

    add logs in OrderController

--- src/main/java/com/example/ecommerce/controllers/OrderController.java
+++ src/main/java/com/example/ecommerce/controllers/OrderController.java
@@ -1,10 +1,14 @@
 package com.example.ecommerce.controllers;
 
 
+import com.example.ecommerce.exception.UsernameNotFoundException;
 import com.example.ecommerce.model.persistence.User;
 import com.example.ecommerce.model.persistence.UserOrder;
 import com.example.ecommerce.model.persistence.repositories.OrderRepository;
 import com.example.ecommerce.model.persistence.repositories.UserRepository;
+import com.example.ecommerce.util.LogMF;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,32 +18,45 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
-	
-	
+
+	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Autowired
 	private OrderRepository orderRepository;
-	
-	
+
+
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+		log.debug(LogMF.format("submit", "Attempting create order.", "username", username));
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
-			return ResponseEntity.notFound().build();
+			String msg = "Order create failed: username not found.";
+			log.debug(LogMF.format("submit", msg, "username", username));
+			throw new UsernameNotFoundException(msg);
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		log.debug(LogMF.format("submit", "Success: Order created.", order));
 		return ResponseEntity.ok(order);
 	}
-	
+
 	@GetMapping("/history/{username}")
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
+		log.debug(LogMF.format("getOrdersForUser", "Attempting to get order.", "username", username));
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
-			return ResponseEntity.notFound().build();
+			log.debug(LogMF.format("getOrdersForUser", "Invalid username.", "username", username));
+			throw new UsernameNotFoundException();
 		}
-		return ResponseEntity.ok(orderRepository.findByUser(user));
+		List<UserOrder> orders = orderRepository.findByUser(user);
+		if (orders.isEmpty()) {
+			log.debug(LogMF.format("getOrdersForUser", "No orders found.", orders));
+		} else {
+			log.debug(LogMF.format("getOrdersForUser", "Success: orders found.", orders));
+		}
+		return ResponseEntity.ok(orders);
 	}
 }
