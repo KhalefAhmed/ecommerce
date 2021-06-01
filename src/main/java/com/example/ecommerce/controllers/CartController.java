5466965ad9b10110f3e52f4459f623336f99a19b
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-01 22:43:19 +0200

    logs in PostMapping (/addToCart)

--- src/main/java/com/example/ecommerce/controllers/CartController.java
+++ src/main/java/com/example/ecommerce/controllers/CartController.java
@@ -1,6 +1,8 @@
 package com.example.ecommerce.controllers;
 
 
+import com.example.ecommerce.exception.ItemNotFoundException;
+import com.example.ecommerce.exception.UsernameNotFoundException;
 import com.example.ecommerce.model.persistence.Cart;
 import com.example.ecommerce.model.persistence.Item;
 import com.example.ecommerce.model.persistence.User;
@@ -8,6 +10,9 @@ import com.example.ecommerce.model.persistence.repositories.CartRepository;
 import com.example.ecommerce.model.persistence.repositories.ItemRepository;
 import com.example.ecommerce.model.persistence.repositories.UserRepository;
 import com.example.ecommerce.model.requests.ModifyCartRequest;
+import com.example.ecommerce.util.LogMF;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +27,9 @@ import java.util.stream.IntStream;
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(CartController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -31,21 +38,25 @@ public class CartController {
 	
 	@Autowired
 	private ItemRepository itemRepository;
-	
+
 	@PostMapping("/addToCart")
-	public ResponseEntity<Cart> addTocart(@RequestBody ModifyCartRequest request) {
+	public ResponseEntity<Cart> addToCart(@RequestBody ModifyCartRequest request) {
+		log.debug(LogMF.format("addToCart", "Adding item(s) to cart.", request));
 		User user = userRepository.findByUsername(request.getUsername());
 		if(user == null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			log.debug(LogMF.format("addToCart", "Invalid user id.", request));
+			throw new UsernameNotFoundException("User '" + request.getUsername()+ "' does not exist.");
 		}
 		Optional<Item> item = itemRepository.findById(request.getItemId());
 		if(!item.isPresent()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			log.debug(LogMF.format("addToCart", "Invalid item id.", request));
+			throw new ItemNotFoundException("Item id '" + request.getItemId() +"' does not exist.");
 		}
 		Cart cart = user.getCart();
 		IntStream.range(0, request.getQuantity())
-			.forEach(i -> cart.addItem(item.get()));
+				.forEach(i -> cart.addItem(item.get()));
 		cartRepository.save(cart);
+		log.debug(LogMF.format("addToCart", "Success: item(s) added.", item.get()));
 		return ResponseEntity.ok(cart);
 	}
 	
