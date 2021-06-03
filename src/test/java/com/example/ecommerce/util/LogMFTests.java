e70b886ed9b1300f74199a2187d40d66b43f885d
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-04 00:32:13 +0200

    test_format_pojo logMF

--- src/test/java/com/example/ecommerce/util/LogMFTests.java
+++ src/test/java/com/example/ecommerce/util/LogMFTests.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.util;
 
+import com.example.ecommerce.model.requests.CreateUserRequest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,6 +13,19 @@ public class LogMFTests {
         //String expectedJson = "{ \"method\" : \"myMethod\", \"message\": \"My Message\", { \"java.lang.String\" : {\"MyObject\" : \"My Value\" } }";
         String expected = "method=[myMethod] message=[My Message] object=[java.lang.String] MyObject=[My Value]";
 
+        assertEquals(expected, message);
+    }
+
+    @Test
+    public void test_format_pojo(){
+        CreateUserRequest request = new CreateUserRequest();
+        request.setUsername("test");
+        request.setPassword("password");
+        request.setConfirmPassword("confirm");
+        String message = LogMF.format("myMethod", "My Message", request);
+        String expected = "method=[myMethod] message=[My Message] " +
+                "object=[com.example.ecommerce.model.requests.CreateUserRequest] " +
+                "password=[***** CONFIDENTIAL *****] confirmPassword=[***** CONFIDENTIAL *****] username=[test]";
         assertEquals(expected, message);
     }
 }
