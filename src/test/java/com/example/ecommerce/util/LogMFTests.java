ca5de68cf2d633741482ea7047b27a5a78aa8bef
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-04 23:14:57 +0200

    add splunk screenshots (createUser Request failure

--- src/test/java/com/example/ecommerce/util/LogMFTests.java
+++ src/test/java/com/example/ecommerce/util/LogMFTests.java
@@ -11,7 +11,6 @@ public class LogMFTests {
     @Test
     public void test_format(){
         String message = LogMF.format("myMethod", "My Message", "MyObject", "My Value");
-        //String expectedJson = "{ \"method\" : \"myMethod\", \"message\": \"My Message\", { \"java.lang.String\" : {\"MyObject\" : \"My Value\" } }";
         String expected = "method=[myMethod] message=[My Message] object=[java.lang.String] MyObject=[My Value]";
 
         assertEquals(expected, message);
